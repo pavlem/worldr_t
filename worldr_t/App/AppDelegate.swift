9f1cb40f56e5b037492df01b3f351a6420872786
Pavle Mijatovic <paul.mijatovic@sprintingsoftware.com>
2021-06-11 11:05:51 +0200

    Initial

---
+++ worldr_t/App/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  worldr_t
+//
+//  Created by Pavle Mijatovic on 11.6.21..
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ worldr_t/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  worldr_t
+//
+//  Created by Pavle Mijatovic on 11.6.21..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
